419481d33f95ec74a5e59ba929e3ca3b6908a77c
egorova <egorova.fingers@gmail.com>
2020-05-27 00:06:35 +0300

    Added search functionality for craigslist site.

--- ws_app/urls.py
+++ ws_app/urls.py
@@ -4,4 +4,5 @@ from ws_app import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("new_search", views.new_search, name="new_search"),
 ]
--- ws_app/views.py
+++ ws_app/views.py
@@ -1,4 +1,49 @@
+from urllib.parse import quote_plus
+
 from django.shortcuts import render
+from bs4 import BeautifulSoup
+import requests
+
+from ws_app import models
+
+
+BASE_CRAIGSLIST_URL = "https://losangeles.craigslist.org/search/?query={}"
+BASE_IMAGE_URL = "https://images.craigslist.org/{}_300x300.jpg"
+DEFAULT_IMAGE_URL = 'https://craigslist.org/images/peace.jpg'
+DEFAULT_PRICE = "N/A"
+
 
 def home(request):
     return render(request, "base.html")
+
+def new_search(request):
+    search = request.POST.get("search")
+    models.Search.objects.create(search=search)
+    final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
+    response = requests.get(final_url)
+    data = response.text
+
+    soup = BeautifulSoup(data, features="html.parser")
+    post_listings = soup.find_all("li", {"class": "result-row"})
+
+    final_postings = []
+    for post in post_listings:
+        post_title = post.find(class_="result-title").text
+        post_url = post.find("a").get("href")
+        if post.find(class_="result-price"):
+            post_price = post.find(class_="result-price").text
+        else:
+            post_price = DEFAULT_PRICE
+        if post.find(class_="result-image").get("data-ids"):
+            post_image_id = post.find(class_="result-image").get("data-ids").split(",")[0].split(':')[1]
+            post_image_url = BASE_IMAGE_URL.format(post_image_id)
+        else:
+            post_image_url = DEFAULT_IMAGE_URL
+        # TODO refactor this
+        final_postings.append((post_title, post_url, post_price, post_image_url))
+
+    response_data = {
+        "search": search,
+        "final_postings": final_postings,
+    }
+    return render(request, "ws_app/new_search.html", response_data)
